8e116ce162d38cfc5e9a59e1d5de11598715766c
Marko Gusic <markogusic@aol.com>
2018-10-02 16:39:45 -0700

    final code

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burger (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50),
+    burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT VALUE FALSE,
     PRIMARY KEY (id)
 );
